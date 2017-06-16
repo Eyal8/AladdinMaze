ae55d564a25cb255047bf6999d75a40a5e6d295e
eyal8_000 <eyal8arviv@gmail.com>
2017-06-16 13:12:35 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class Controller {
+    public Button btn_1;
+    public void ButtonOnAction(ActionEvent actionEvent) {
+        btn_1.setText("SHMULIK");
+    }
+}
